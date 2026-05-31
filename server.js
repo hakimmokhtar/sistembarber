01592f342b3c89c8a9fb4d392d807fd12bfb6956
Hakim Mokhtar <hakimmokhtar1998@gmail.com>
2026-06-01 01:27:39 +0800

    guna public url railway

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 
 
 // Ganti kod db pool kau dengan URL dari Railway tadi
-const db = mysql.createPool('mysql://root:GryyxyzwLNvmWnHgDNUuGSpcCmNrmZlz@zephyr.proxy.rlwy.net:56034/railway');
+const db = mysql.createPool('mysql://root:GryyxyzwLNvmWnHgDNUuGSpcCmNrmZlz@mysql.railway.internal:3306/railway');
 
 // Test sambungan
 db.getConnection((err, conn) => {
